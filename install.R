8b2df2b4810413259e86671852b1b764884f0021
Thiagolopes <l>
2025-09-21 01:10:35 -0300

    Limpa repositório e mantém apenas os principais scripts

--- install.R
+++
@@ -1,8 +0,0 @@
-pkgs <- c(
-  "gfwr","dplyr","tidyr","purrr","tibble","stringr","readr","lubridate",
-  "sf","units","httr","jsonlite","glue","ggplot2","stars","raster","ncdf4","rerddap", "rnaturalearth", "rnaturalearthdat", "scales")
-install.packages("scales")
-
-
-new <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
-if (length(new)) install.packages(new, repos = "https://cloud.r-project.org")
